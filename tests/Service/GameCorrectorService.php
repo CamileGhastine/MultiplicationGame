265315a5b8de857b2e20b27ba7cbffc36b98407e
CamileGhastine <camileghastine@hotmail.com>
2022-01-06 09:37:14 +0100

    add the document of conception and passation
    Doc de passation.pdf and doc de passation.odt

---
+++ tests/Service/GameCorrectorService.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Service\GameCorrector;
+use App\Entity\Game;
+
+class GameCorrectorTest extends TestCase {
+
+    private Game $game;
+    private GameCorrector $gameCorrector;
+
+    // init
+    public function setUp() :void {
+        $this->game = new Game;
+        $this->gameCorrector = new GameCorrector();
+    }
+
+    // test instance of game
+    public function testInstanceGame() {
+        $this->assertInstanceOf(Game::class, $this->game);
+    }
+
+    // test instance of gameCorrector
+    public function testInstanceGameCorrector() {
+        $this->assertInstanceOf( GameCorrector::class, $this->gameCorrector );
+    }
+    
+}
